435ba010601bfe77c57df011122464bf0ac99390
pilarvermu <pilaribermu@gmail.com>
2025-02-01 22:34:09 +0100

    Añadido UI y funcionalidad reset

--- script.js
+++ script.js
@@ -17,13 +17,15 @@ const gameBoard = (function(){
         return board;
     }
 
+
+    //PARA VERSIÓN CONSOLA:
     //imprimir tablero (al ser navegador web y usar consolelog con getBoard,
     //se estara guardando una referencia y acabara impriminedo la version final
     //por eso hay q hacer una copia)
-    function printBoard() {
-        const boardCopy = gameBoard.getBoard().map(row => [...row]);
-        console.log(boardCopy);
-    }
+    //function printBoard() {
+    //    const boardCopy = gameBoard.getBoard().map(row => [...row]);
+    //    console.log(boardCopy);
+    //}
     
 
     //colocar token en tablero
@@ -35,7 +37,9 @@ const gameBoard = (function(){
         return false;
     };
 
-    return {getBoard, placeToken, printBoard}
+    //PARA VERSIÓN CONSOLA:
+    //return {getBoard, placeToken, printBoard}
+    return {getBoard, placeToken};
 })();
 
 //jugadores, guardan token (O ó X)
@@ -49,12 +53,17 @@ const gameController = (function(){
     const player1 = createPlayer("X");
     const player2 = createPlayer("O");
     let currentPlayer = player1;
+    let gameOver = false;
 
+
+    //VERSIÓN CONSOLA
+    /*
     function getPlayerInput() {
         const row = prompt(`Jugador ${currentPlayer.token}, ingresa la fila (0, 1, 2):`);
         const col = prompt(`Jugador ${currentPlayer.token}, ingresa la columna (0, 1, 2):`);
         return { row: parseInt(row), col: parseInt(col) }; // Devolver un objeto con las coordenadas
     }
+    */
 
     //cambio de turno, sería función privada usada solo por playRound
     function switchTurns(){
@@ -63,21 +72,54 @@ const gameController = (function(){
 
 
     //si el turno ha sido valido se vuelve a mostrar el tablero actualizado
-    function playRound(){
-        const { row, col } = getPlayerInput();
+    function playRound(row, col){
+        //const { row, col } = getPlayerInput();
+
+        //si el juego ha terminado no permite seguir jugando
+        if(gameOver) return;
 
         if(gameBoard.placeToken(row,col,currentPlayer.token)){
-            console.log(`Jugador ${currentPlayer.token} juega en (${row}, ${col})`);
+            //console.log(`Jugador ${currentPlayer.token} juega en (${row}, ${col})`);
             //console.log(gameBoard.getBoard());
-            gameBoard.printBoard();
+            //gameBoard.printBoard();
+
             switchTurns();
+            screenCtrl.updateScreen();
+
+            switch(checkWinner()){
+                case "X":
+                setTimeout(() => alert("Ganan las X"), 100); // Usar setTimeout para mostrar la alerta después
+                gameOver=true;
+                break;
+                case "O":
+                setTimeout(() => alert("Ganan las O"), 100); // Usar setTimeout para mostrar la alerta después
+                gameOver = true;
+                break;
+                case "empate":
+                setTimeout(() => alert("Es un empate"), 100); // Usar setTimeout para mostrar la alerta después
+                gameOver=true;
+                break;
+                default:
+                // El juego continúa
+                break;
+            }
+
+            
         }else{
-            console.log("Casilla ocupada. Intenta otra vez");
+            alert("Casilla ocupada. Intenta otra vez");
         }
         
     };
 
+    function getCurrentPlayer(){
+        return currentPlayer;
+    }
 
+    function getGameOver(){
+        return gameOver;
+    }
+
+    
     function checkWinner(){
         const board = gameBoard.getBoard();
 
@@ -114,48 +156,121 @@ const gameController = (function(){
 
         return "empate"; // Empate si no hay casillas vacías
     }
+    
         
     
 
 
+    /*
+    //SI EL JUEGO NECESITASE INPUTS: (al hacerlo con UI los gestionamos diferente)
+    function startGame() {
+        let gameActive = true; 
 
-//SI EL JUEGO NECESITASE INPUTS: (al hacerlo con UI los gestionamos diferente)
-function startGame() {
-    let gameActive = true; 
-
-    while(gameActive) {
-        playRound(); 
-        
-        //verificar fin del juego
-        switch(checkWinner()){
-            case "X":
-                console.log("¡El ganador es el jugador X!");
-                gameActive = false;
-                break;
-            case "O":
-                console.log("¡El ganador es el jugador O!");
-                gameActive = false;
-                break;
-            case "empate":
-                console.log("¡Es un empate!");
-                gameActive = false;
-                break;
-            default:
-                // El juego continúa
-                break;
+        while(gameActive) {
+            playRound(); 
+            
+            //verificar fin del juego
+            switch(checkWinner()){
+                case "X":
+                    console.log("¡El ganador es el jugador X!");
+                    gameActive = false;
+                    break;
+                case "O":
+                    console.log("¡El ganador es el jugador O!");
+                    gameActive = false;
+                    break;
+                case "empate":
+                    console.log("¡Es un empate!");
+                    gameActive = false;
+                    break;
+                default:
+                    // El juego continúa
+                    break;
+            }
+            
         }
-         
     }
-}
+    */
 
 
-    return{ startGame};
+    //Función de reset
+    function resetGame(){
+        gameBoard.getBoard().forEach(row => row.fill("-"));
+        currentPlayer = player1;
+        gameOver = false;
+        screenCtrl.updateScreen();
+
+    }
+
+    return{playRound, getCurrentPlayer, resetGame, getGameOver};
 
 
 })();
 
-
-gameController.startGame();
+//gameController.startGame();
 
 //startGame();
 
+//PARTE UI
+function screenController(){
+    const playerTurnDiv = document.querySelector(".turn");
+    const boardDiv = document.querySelector(".board");
+    const resetButton = document.querySelector(".reset-button");
+
+    //Actualizar la interfaz
+    function updateScreen(){
+        //limpiar tablero
+        boardDiv.innerHTML =" ";
+
+        //obtener el estado más reciente del tablero y el jugador actual
+        const board = gameBoard.getBoard();
+        const activePlayer = gameController.getCurrentPlayer();
+
+        //mostrar el turno del jugador
+        playerTurnDiv.textContent = gameController.getGameOver() ? "Juego Terminado" : `${activePlayer.token}'s turn...`;
+    
+        //renderizar las celdas del tablero
+        board.forEach((row, rowIndex)=>{
+            row.forEach((cell,colIndex)=>{
+                const cellButton = document.createElement("button");
+                cellButton.classList.add("cell");
+                cellButton.dataset.row = rowIndex;
+                cellButton.dataset.col = colIndex;
+                cellButton.textContent = cell === "-" ? "" : cell;  //la primera vez que recorramos el tablero queremos cambiar los "-" del tablero por consola a espacios en blanco
+                if(cell !== "-"){
+                    cellButton.classList.add(cell);
+                }
+                boardDiv.appendChild(cellButton);
+            })
+        })
+    }
+
+    //click en el tablero
+    function clickHandlerBoard(e){
+        const selectedRow = e.target.dataset.row;
+        const selectedCol = e.target.dataset.col;
+
+        // Asegurarnos de que se ha hecho clic en una celda y no en un espacio vacío
+        if (selectedRow === undefined || selectedCol === undefined) return;
+
+        gameController.playRound(parseInt(selectedRow), parseInt(selectedCol));
+        //updateScreen()
+    }
+
+    boardDiv.addEventListener("click", clickHandlerBoard);
+
+
+    //click en el botón de reset
+    resetButton.addEventListener("click", ()=>{
+        gameController.resetGame();
+}); 
+
+    //renderizar el tablero al inicio
+    updateScreen();
+
+    return{updateScreen};
+    
+}
+
+//inicializar el controlador de pantalla
+const screenCtrl = screenController();
